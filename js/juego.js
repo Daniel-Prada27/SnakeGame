8081b1ca5107a7f57932b19da2b3ed624978edb9
Daniel <danielspradah0@gmail.com>
2023-10-08 22:03:24 -0500

    Create logic to move down

--- js/juego.js
+++ js/juego.js
@@ -1,18 +1,29 @@
 let board = document.getElementById('game-board');
 
-let snakeArr = [];
-let k = 1;
+let snakeArr = board.childNodes;
+let head;
+
+function createStartNode() {
+    let node = document.createElement('div');
+    node.style.backgroundColor = "red"
+    setRowStart(node, 11)
+    setRowEnd(node, 12)
+    setColumnStart(node, 11)
+    setColumnEnd(node, 12)
+
+    boardAppend(node);
+}
+
+function boardAppend(node) {
+    board.appendChild(node);
+    head = snakeArr[snakeArr.length - 1]
+}
+
 function createNode() {
     let node = document.createElement('div');
     node.style.backgroundColor = "red"
-    setRowStart(node, k)
-    setRowEnd(node, k)
-    setColumnStart(node, k)
-    setColumnEnd(node, k)
-
-    snakeArr.push(node);
-    board.appendChild(node)
-    k++
+    return node;
+    // board.appendChild(node)
 }
 
 function setRowStart(node, rowStart) {
@@ -29,4 +40,18 @@ function setColumnStart(node, columnStart) {
 
 function setColumnEnd(node, columnEnd) {
     node.style.gridColumnEnd = columnEnd;
+}
+
+function removeTail() {
+    board.removeChild(snakeArr[0]);
+}
+
+function moveHeadDown() {
+    let newHead = createNode();
+    setRowStart(newHead, parseInt(head.style.gridRowStart) + 1)
+    setRowEnd(newHead, parseInt(head.style.gridRowEnd) + 1)
+    setColumnStart(newHead, head.style.gridColumnStart)
+    setColumnEnd(newHead, head.style.gridColumnStart)
+
+    boardAppend(newHead);
 }
