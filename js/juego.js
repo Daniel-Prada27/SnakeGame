a4dc242b24673e3f5b213f916962b016e86c2be8
Daniel <danielspradah0@gmail.com>
2023-10-28 21:09:58 -0500

    Fix movement boost when repeating movement input
    Cuando la serpiente se mueve hacia arriba, por ejemplo, y el usuario mantiene presionado hacia arriba, la serpiente acelera. Se ha corregido ese comportamiento.

--- js/juego.js
+++ js/juego.js
@@ -233,16 +233,16 @@ function sumPoints() {
 
 window.addEventListener('keydown', (event) => {
     let key = event.code;
-    if (key == 'ArrowRight' && direction != 'left') {
+    if (key == 'ArrowRight' && direction != 'left' && direction != 'right') {
         // moveHeadRight();
         goRight();
-    } else if (key == 'ArrowLeft' && direction != 'right' ) {
+    } else if (key == 'ArrowLeft' && direction != 'right' && direction != 'left') {
         // moveHeadLeft();
         goLeft();
-    } else if (key == 'ArrowUp' && direction != 'down') {
+    } else if (key == 'ArrowUp' && direction != 'down' && direction != 'up') {
         // moveHeadUp();
         goUp();
-    } else if (key == 'ArrowDown' && direction != 'up') {
+    } else if (key == 'ArrowDown' && direction != 'up' && direction != 'down') {
         // moveHeadDown();
         goDown();
     }
